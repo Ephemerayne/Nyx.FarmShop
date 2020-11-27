e63fdcbaa8a83bdbc80b14779bae069ca9630537
rockstar4095 <rockstar4095@gmail.com>
2020-11-27 20:18:19 +0300

    added fragment menu

--- app/src/main/java/space/lala/nyxfarmshop/MainActivity.java
+++ app/src/main/java/space/lala/nyxfarmshop/MainActivity.java
@@ -3,10 +3,13 @@ package space.lala.nyxfarmshop;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -26,8 +29,10 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.item_1:
+
                         break;
                     case R.id.item_2:
+                        openFragment(new MenuFragment());
                         break;
                     case R.id.item_3:
                     case R.id.item_4:
@@ -36,6 +41,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+    }
+    public void openFragment (Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
