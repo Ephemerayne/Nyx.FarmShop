c3b967ca7128164ce1970c7f1c527f0b0965511e
rockstar4095 <rockstar4095@gmail.com>
2020-12-01 18:30:10 +0300

    added recycler view

--- app/src/main/java/space/lala/nyxfarmshop/MarketFragment.java
+++ app/src/main/java/space/lala/nyxfarmshop/MarketFragment.java
@@ -2,8 +2,9 @@ package space.lala.nyxfarmshop;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -11,11 +12,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import space.lala.nyxfarmshop.model.MarketItem;
+
 public class MarketFragment extends Fragment {
 
     private static final int NUM_PAGES = 5;
     private ViewPager2 viewPager;
     private FragmentStateAdapter pagerAdapter;
+    private RecyclerView recyclerView;
+    private CustomAdapterRecyclerView recyclerViewAdapter;
 
     @Override
     public View onCreateView(
@@ -26,8 +33,31 @@ public class MarketFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_market, container, false);
 
         viewPager = view.findViewById(R.id.view_pager);
+        recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
         pagerAdapter = new ScreenSlidePagerAdapter(this, NUM_PAGES);
         viewPager.setAdapter(pagerAdapter);
+
+        recyclerViewAdapter = new CustomAdapterRecyclerView();
+        recyclerView.setAdapter(recyclerViewAdapter);
+        recyclerViewAdapter.setItems(getMarketItems());
+
         return view;
+    }
+
+    private ArrayList<MarketItem> getMarketItems() {
+        final ArrayList<MarketItem> marketItems = new ArrayList<>();
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        marketItems.add(new MarketItem("item"));
+        return marketItems;
     }
 }
---
+++ app/src/main/java/space/lala/nyxfarmshop/model/MarketItem.java
@@ -0,0 +1,10 @@
+package space.lala.nyxfarmshop.model;
+
+public class MarketItem {
+    public String itemName;
+    public int itemImageId;
+
+    public MarketItem(String itemName) {
+        this.itemName = itemName;
+    }
+}
